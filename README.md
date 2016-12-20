43ca276919d7f8da17c9870dcf9aee0bebf0bb3b
Anton Akentiev <anthony.akentiev@gmail.com>
2016-12-20 17:28:29 +0300

    Contract review. Commit 1

--- README.md
+++ README.md
@@ -9,10 +9,10 @@ that through its Directors and Founders, has duly authorized one billion shares 
 
 that it has authorized the minting of ten billion digital tokens or coins created on the Ethereum Blockchain called bitqy and, further certifies,
 
-that the holder of this bitqy token, is also the holder of one-tenth of a share of bitqyck, Inc. Common Stock, and further certifies,
+that the holder of this BitQy token, is also the holder of one-tenth of a share of bitqyck, Inc. Common Stock, and further certifies,
 
-that the holder of this coin shall enjoy the rights and benefits as a shareholder of bitqyck, Inc. pursuant to the terms and conditions that the Directors and Founders attach to the holders and thereafter, pursuant to the terms and conditions that those holding a winning margin of bitqy deem proper or necessary and, further certifies,
+that the holder of this coin shall enjoy the rights and benefits as a shareholder of bitqyck, Inc. pursuant to the terms and conditions that the Directors and Founders attach to the holders and thereafter, pursuant to the terms and conditions that those holding a winning margin of BitQy deem proper or necessary and, further certifies,
 
-that this bitqy token is freely transferable by the holder hereof in any manner, which said holder deems appropriate and reasonable. 
+that this BitQy token is freely transferable by the holder hereof in any manner, which said holder deems appropriate and reasonable. 
 
 The holder of this Bitqy token certifies that he or she has ownership and possession pursuant to a legal transaction or transfer from the prior holder. 
--- token.sol
+++ token.sol
@@ -1,26 +1,63 @@
+/**
+ * Reviewed:
+ * Current version is 0.4.7 (20th of December). Please update.
+ */
 pragma solidity ^0.4.4;
 
 import 'Mortal.sol';
 import 'ERC20.sol';
 import 'SafeMath.sol';
 
-
+            
+/**
+ * Reviewed: Mortal should be removed? See comments in Mortal.sol
+ *
+ * Security review metodology is here - http://ethereum.stackexchange.com/questions/8551/methodological-security-review-of-a-smart-contract
+ */
 contract Token is Mortal, ERC20, SafeMath {
-
+    /**
+     * Reviewed: Better to move 'balances' to ERC20 contract
+     */
     mapping(address => uint) balances;
+
     bool public tradeable = false;
 
+    /**
+     * Reviewed: Better to move this method to ERC20
+     *
+     * Mixing safe math (that throws) with return true/false is not a good coding style.
+     * Better always to have one return strategy (in this case no throws). See 
+     * See this gist here - https://gist.github.com/AnthonyAkentiev/14791dee17996c852ecd510ff3cdc066
+     * 
+     * + Interger Overflow = OK, Checked
+     */
     function transfer(address _to, uint _value) returns (bool success) {
-        if (msg.sender == owner || tradeable == true) {
+        // BUG! in the original ERC20 contract transfer should be callable BY ANY token holder.
+        // 1) anyway only the owner has initial shares 
+        // 2) if someone bought shares -> then he should be able to move them to anyone else (or to his other accounts)
+
+        // so please remove (msg.sender==owner) check
+        // or leave it - if it's as intended
+        // 
+        // But see: 
+        // "That this BitQy token is freely transferable by the holder hereof in any manner, 
+        //  which said holder deems appropriate and reasonable".
+        if ((msg.sender == owner) || (tradeable == true)) {
             balances[msg.sender] = safeSub(balances[msg.sender], _value);
             balances[_to] = safeAdd(balances[_to], _value);
+
+            // calling an event
             Transfer(msg.sender, _to, _value);
+
             return true;
         } else {
             return false;
         }
     }
 
+    /**
+     * Reviewed: Better to move this method to ERC20
+     */
     function balanceOf(address _owner) constant returns (uint balance) {
         return balances[_owner];
     }
