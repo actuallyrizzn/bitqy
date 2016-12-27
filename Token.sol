ddb43bca8487c62fd24460dd962c90d821d85b8d
Cody Marx Bailey <superphly@gmail.com>
2016-12-27 00:11:44 -0600

    I’m bringing “Token” back

---
+++ Token.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.4.7;
+
+import 'ERC20.sol';
+
+contract Token is ERC20 {
+    uint256 public initialSupply;
+    mapping(address => uint) balances;
+
+    function transfer(address _to, uint256 _value) returns (bool success) {
+        if (balances[msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
+            balances[msg.sender] -= _value;
+            balances[_to] += _value;
+            Transfer(msg.sender, _to, _value);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function balanceOf(address _owner) constant returns (uint balance) {
+        return balances[_owner];
+    }
+
+    function totalSupply() constant returns (uint256) {
+        return initialSupply;
+    }
+}
