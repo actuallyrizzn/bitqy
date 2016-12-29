93e5cd8eaa03eaf4731a3b2bfecb0951680da4ac
Anton Akentiev <anthony.akentiev@gmail.com>
2016-12-29 13:55:50 +0300

    Contract review #2

--- Token.sol
+++ Token.sol
@@ -2,35 +2,59 @@ pragma solidity ^0.4.7;
 
 import 'ERC20.sol';
 
+/**
+ * Reviewed:
+ * Added comments (see below)
+ */
 contract Token is ERC20 {
-    uint256 public initialSupply;
-    mapping(address => uint) balances;
+    uint256 public initialSupply = 0;
+    // Style: changed from uint to uint256
+    mapping(address => uint256) balances;
 
-    /* Note: I have this transfer function marked as internal
-    	to prevent it from being called publically since it's
-    	not using the tradable modifier that I'm using in the
-    	BitQy.transfer override.
+    /**
+     * Reviewed: good
+     * Added comments (see below)
+     * Fixed return style
+     * Interger Overflow = OK, Checked
 
-    	I'm not sure if this is correct.
-
-    	Also, should balances be here or in BitQy contract?
-    */
-    function transfer(address _to, uint256 _value) internal returns (bool success) {
-        if (balances[msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
+     * I changed the behaviour. 'transfer' from BitQy overrides current 'tranfer' method.
+     */
+    function transfer(address _to, uint256 _value) returns (bool success) {
+        // instead of using SafeMath - we use explicit checking 
+        if ((balances[msg.sender] >= _value) && ((balances[_to] + _value) > balances[_to])) {
             balances[msg.sender] -= _value;
             balances[_to] += _value;
+
             Transfer(msg.sender, _to, _value);
-            return true;
+            success = true;
+
+            //return true;
         } else {
-            return false;
+            success = false;
+
+            //return false;
         }
+
+        return;
     }
 
-    function balanceOf(address who) constant returns (uint256) {
-        return balances[who];
+    /**
+     * Reviewed: goood. Changed ret value style only
+     */
+    function balanceOf(address who) constant returns (uint256 out) {
+        out = balances[who];
+        return;
+
+        // return balances[who];
     }
 
-    function totalSupply() constant returns (uint256) {
-        return initialSupply;
+    /**
+     * Reviewed: goood. Changed ret value style only
+     */
+    function totalSupply() constant returns (uint256 out) {
+        out = initialSupply;
+        return;
+
+        // return initialSupply;
     }
 }
