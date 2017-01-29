ab48b48c202855997f3baf319d06d7a5e824ab12
Cody Marx Bailey <superphly@gmail.com>
2017-01-29 15:54:01 -0600

    no message

--- ERC20.sol
+++ ERC20.sol
@@ -1,11 +1,11 @@
-pragma solidity ^0.4.7;
+pragma solidity ^0.4.8;
 
 contract ERC20 {
     function totalSupply() constant returns (uint256 totalSupply);
     function transfer(address to, uint256 value) returns (bool success);
     function balanceOf(address who) constant returns (uint256);
 
-	function transferFrom(address _from, address _to, uint256 _value) returns (bool success);
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool success);
     function approve(address _spender, uint256 _value) returns (bool success);
     function allowance(address _owner, address _spender) constant returns (uint256 remaining);
 
