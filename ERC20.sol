ad9ca76fd45b086e09adc42d3bf84705b28efe05
Mark Rizzn Hopkins <guesswho@rizzn.com>
2017-05-08 16:53:05 -0500

    Add files via upload
    Caudicum pulled their copy of bitqy from their repo. Adding it here for a permanent record.
    
    This was the "Doctor Bitcoin" endorsed version of the token that was only ever deployed on TestNet.
    
    File dates show last updates occurred in Early Feb, with minor updates in early March.

--- ERC20.sol
+++ ERC20.sol
@@ -1,13 +1,14 @@
-pragma solidity ^0.4.4;
-
+pragma solidity ^0.4.9;
 
 contract ERC20 {
+    function totalSupply() constant returns (uint256 totalSupply);
+    function transfer(address to, uint256 value) returns (bool success);
+    function balanceOf(address who) constant returns (uint256);
 
-    uint public totalSupply;
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool success);
+    function approve(address _spender, uint256 _value) returns (bool success);
+    function allowance(address _owner, address _spender) constant returns (uint256 remaining);
 
-    function balanceOf(address who) constant returns (uint);
-    function transfer(address to, uint value) returns (bool);
-
-    event Transfer(address indexed from, address indexed to, uint value);
-
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 }
